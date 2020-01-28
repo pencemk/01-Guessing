ab1fa481598ae921b8c5c12942040b719d371089
pencemk <59931457+pencemk@users.noreply.github.com>
2020-01-27 23:55:48 -0500

    I have successfully updated this file to fit the requirements for my C220 class at Indiana University! Hope you can enjoy my number guessing game.

--- main.py
+++ main.py
@@ -2,3 +2,33 @@ import sys, random
 
 assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
+
+quit = False 
+range = 100 
+
+while not quit: 
+    random_number = random.randint(1,range)
+    count = 2
+    number = -1
+    while number != random_number:
+        number = input("Hello! PLease guess a number between 1 and {}: ".format(range))
+        if not number.isdigit(): 
+            print("Please guess a number.")
+        else:
+            number = int(number)
+            count = count + 1 
+            print("Sorry, you were incorrect.")
+            if number > random_number:
+                print("Too high.")
+            else number < random_number:
+                print ("That guess was too low.")
+print("Well done!")
+print("You guessed it in {} attempts." .format(count))
+play_again = input("\nWould you like to try again? (yes or no)? ")
+play_again = play_again. lower()
+if play_again == "yes" or play_again == "y":
+    quit=False
+else: 
+    quit=true
+
+print("Thank you for playing!")
